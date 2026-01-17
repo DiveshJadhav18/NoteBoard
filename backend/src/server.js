c99700f8f6c80bc4c0b57dd51beb9444bdb0db2b
Divesh <diveshjadhav048@gmail.com>
2026-01-17 16:09:07 +0530

    Prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path';
 
 import notesRoutes from "./routes/noteRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -10,13 +11,17 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 //middleWare
-app.use(
+if (process.env.NODE_ENV !== "production") {
+ app.use(
     cors({
         origin: "http://localhost:5173",
+        credentials: true,
     })
-);
+);   
+}
 
 app.use(express.json()); //this middleware parse json bodies
 
@@ -26,7 +31,23 @@ app.use(express.json()); //this middleware parse json bodies
 //     next();
 // });
 
-app.use("/api/notes", rateLimiter, notesRoutes);
+if (process.env.NODE_ENV === "production") {
+  app.use("/api/notes", rateLimiter, notesRoutes);
+} else {
+  app.use("/api/notes", notesRoutes);
+}
+
+
+if (process.env.NODE_ENV === "production") {
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.use((req, res) => {
+  res.sendFile(
+    path.join(__dirname, "../frontend", "dist", "index.html")
+  );
+});
+
+}
 
 connectDB().then(()=>{
     app.listen(port, ()=>{
